0bdfa838e01236b633574aca7e0cf14549bed696
Dmitry <dzmitry.babichev@gmail.com>
2021-10-29 20:10:52 +0300

    HomeWork 17(to do list)

---
+++ Home Work 17 To do list/frontend/js/models/tasks.js
@@ -0,0 +1,79 @@
+class Tasks {
+  getTasksList() {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('GET', 'http://localhost:3000/api/tasks');
+
+      xhr.onload = () => resolve(JSON.parse(xhr.response));
+
+      xhr.send();
+    });
+  }
+
+  addTask(newTask) {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('POST', 'http://localhost:3000/api/task');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onload = () => resolve(JSON.parse(xhr.response));
+
+      xhr.send(JSON.stringify(newTask));
+    });
+  }
+
+  getTask(id) {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('GET', `http://localhost:3000/api/task/${id}`);
+
+      xhr.onload = () => resolve(JSON.parse(xhr.response));
+
+      xhr.send();
+    });
+  }
+
+  removeTask(id) {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('DELETE', `http://localhost:3000/api/task/${id}`);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onload = () => resolve();
+
+      xhr.send();
+    });
+  }
+
+  editStatusTask(id) {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('PUT', `http://localhost:3000/api/task/${id}/done`);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onload = () => resolve();
+
+      xhr.send();
+    });
+  }
+
+  editTask(updatedTask) {
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('PUT', `http://localhost:3000/api/task/${updatedTask.id}`);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onload = () => resolve();
+
+      xhr.send(JSON.stringify(updatedTask));
+    });
+  }
+}
+
+export default Tasks;
